18b7c9253404b0855b258427cf0820104150a6fc
Luke Martin <lukechile@gmail.com>
2024-01-22 21:30:39 -0500

    fix

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -1,21 +1,5 @@
 import {
-  AmbitLogo,
-  BarepapersLogo,
-  BimLogo,
-  CDGOLogo,
-  ClevertechLogo,
-  ConsultlyLogo,
-  EvercastLogo,
-  Howdy,
-  JarockiMeLogo,
-  JojoMobileLogo,
-  Minimal,
-  MobileVikingsLogo,
-  MonitoLogo,
-  NSNLogo,
-  ParabolLogo,
-  TastyCloudLogo,
-  YearProgressLogo,
+  OrdaosLogo,
 } from "@/images/logos";
 import { GitHubIcon, LinkedInIcon, XIcon } from "@/components/icons";
 
@@ -68,46 +52,36 @@ export const RESUME_DATA = {
   work: [
     {
       company: "Ordaos Bio",
-      link: "https://parabol.co",
+      link: "https://ordaos.bio",
       badges: ["Remote"],
-      title: "Senior Full Stack Developer",
-      logo: ParabolLogo,
+      title: "AI Scientist II",
+      logo: OrdaosLogo,
       start: "2021",
       end: "2024",
       description:
         "Implemented new features, led squad, worked on improving the way developers ship the code, started migration from Emotion to Tailwind CSS and more. Technologies: React, TypeScript, GraphQL",
     },
     {
-      company: "Clevertech",
-      link: "https://clevertech.biz",
+      company: "Ordaos Bio",
+      link: "https://ordaos.bio",
       badges: ["Remote"],
-      title: "Lead Android Developer → Full Stack Developer",
-      logo: ClevertechLogo,
-      start: "2015",
-      end: "2021",
+      title: "AI Scientist I",
+      logo: OrdaosLogo,
+      start: "2021",
+      end: "2024",
       description:
-        "Created Android mobile apps and led teams for companies like Vision Media, DKMS, or AAA. Built live streaming application for Evercast from scratch. Technologies: Android, Kotlin, React, TypeScript, GraphQL",
+        "Implemented new features, led squad, worked on improving the way developers ship the code, started migration from Emotion to Tailwind CSS and more. Technologies: React, TypeScript, GraphQL",
     },
     {
-      company: "Jojo Mobile",
-      link: "https://bsgroup.eu/",
-      badges: [],
-      title: "Android Developer → Lead Android Developer",
-      logo: JojoMobileLogo,
-      start: "2012",
-      end: "2015",
+      company: "Ordaos Bio",
+      link: "https://ordaos.bio",
+      badges: ["Remote"],
+      title: "AI Scientist Intern",
+      logo: OrdaosLogo,
+      start: "2021",
+      end: "2024",
       description:
-        "Built an Android team, created Android apps for biggest Polish companies like LOT, Polskie Radio, Agora, PolskaPress",
-    },
-    {
-      company: "Nokia Siemens Networks",
-      link: "https://www.nokia.com",
-      badges: [],
-      title: "C/C++ Developer",
-      logo: NSNLogo,
-      start: "2010",
-      end: "2012",
-      description: "Creating and testing software for LTE base stations",
+        "Implemented new features, led squad, worked on improving the way developers ship the code, started migration from Emotion to Tailwind CSS and more. Technologies: React, TypeScript, GraphQL",
     },
   ],
   skills: [
@@ -131,7 +105,7 @@ export const RESUME_DATA = {
         "WebRTC",
       ],
       description: "A platform to build and grow your online business",
-      logo: ConsultlyLogo,
+      logo: OrdaosLogo,
       link: {
         label: "consultly.com",
         href: "https://consultly.com/",
@@ -142,7 +116,7 @@ export const RESUME_DATA = {
       techStack: ["Side Project", "TypeScript", "Next.js", "Browser Extension"],
       description:
         "Browser extension that records everything happening in a web application",
-      logo: MonitoLogo,
+      logo: OrdaosLogo,
       link: {
         label: "monito.dev",
         href: "https://monito.dev/",
@@ -153,7 +127,7 @@ export const RESUME_DATA = {
       techStack: ["Side Project", "Next.js", "MDX"],
       description:
         "My personal website and blog. Built with Next.js and Notion API",
-      logo: JarockiMeLogo,
+      logo: OrdaosLogo,
       link: {
         label: "github.com",
         href: "https://jarocki.me/",
@@ -164,7 +138,7 @@ export const RESUME_DATA = {
       techStack: ["Side Project", "Next.js", "Puppeteer"],
       description:
         "Minimalist calendars, habit trackers and planners generator",
-      logo: Minimal,
+      logo: OrdaosLogo,
       link: {
         label: "useminimal.com",
         href: "https://useminimal.com/",
@@ -175,7 +149,7 @@ export const RESUME_DATA = {
       techStack: ["Side Project", "Next.js", "Puppeteer"],
       description:
         "Generates beautiful wallpapers using random shapes and gradients",
-      logo: BarepapersLogo,
+      logo: OrdaosLogo,
       link: {
         label: "barepapers.com",
         href: "https://barepapers.com/",
@@ -185,7 +159,7 @@ export const RESUME_DATA = {
       title: "Year progress",
       techStack: ["Side Project", "TypeScript", "Next.js"],
       description: "Tracks current year progress and displays a countdown",
-      logo: YearProgressLogo,
+      logo: OrdaosLogo,
       link: {
         label: "getyearprogress.com",
         href: "https://getyearprogress.com/",
@@ -202,7 +176,7 @@ export const RESUME_DATA = {
       ],
       description:
         "The Agile meeting co-pilot that delivers better meetings with less effort",
-      logo: ParabolLogo,
+      logo: OrdaosLogo,
       link: {
         label: "github.com",
         href: "https://parabol.co/",
@@ -220,7 +194,7 @@ export const RESUME_DATA = {
       ],
       description:
         "Creative collaboration platform that combines video conferencing and HD media streaming",
-      logo: EvercastLogo,
+      logo: OrdaosLogo,
       link: {
         label: "evercast.us",
         href: "https://www.evercast.us/",
@@ -231,7 +205,7 @@ export const RESUME_DATA = {
       techStack: ["Lead Android Developer", "Android", "Kotlin"],
       description:
         "Android application for leading virtual mobile operator in Poland",
-      logo: MobileVikingsLogo,
+      logo: OrdaosLogo,
       link: {
         label: "mobilevikings.pl",
         href: "https://mobilevikings.pl/",
@@ -242,7 +216,7 @@ export const RESUME_DATA = {
       techStack: ["Lead Android Developer", "Android", "Kotlin"],
       description:
         "Howdy is a place for you to join communities you care about",
-      logo: Howdy,
+      logo: OrdaosLogo,
       link: {
         label: "play.google.com",
         href: "https://howdy.co/",
@@ -253,7 +227,7 @@ export const RESUME_DATA = {
       techStack: ["Lead Android Developer", "Android", "Kotlin"],
       description:
         "Android application for managing and displaying restaurant menus in kiosk mode",
-      logo: TastyCloudLogo,
+      logo: OrdaosLogo,
       link: {
         label: "tastycloud.fr",
         href: "https://www.tastycloud.fr/",
@@ -264,21 +238,21 @@ export const RESUME_DATA = {
       techStack: ["Lead Android Developer", "Android", "Kotlin"],
       description:
         "Android application that helps with sharing your contact details",
-      logo: AmbitLogo,
+      logo: OrdaosLogo,
     },
     {
       title: "Bim",
       techStack: ["Lead Android Developer", "Android", "Kotlin"],
       description:
         "Android application that helps with booking a table in a restaurants",
-      logo: BimLogo,
+      logo: OrdaosLogo,
     },
     {
       title: "Canal Digital GO",
       techStack: ["Lead Android Developer", "Android", "Kotlin"],
       description:
         "Video streaming mobile application for Canal Digital subscribers",
-      logo: CDGOLogo,
+      logo: OrdaosLogo,
     },
   ],
 } as const;
